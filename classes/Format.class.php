a987902de27589ebdee6abeed4d727b7efc28d14
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-04-22 10:51:56 +0200

    Escape usernames with underscore or asterisk

--- classes/Format.class.php
+++ classes/Format.class.php
@@ -41,8 +41,8 @@ class Format {
      */
     static function NoBBC($string) {
         return str_replace(
-            array('[', ']', '#', '<', '>'),
-            array('(', ')',  '', '(', ')'),
+            array('[', ']', '#', '<', '>', '_', '*'),
+            array('(', ')',  '', '(', ')', ' ', ''),
             $string
         );
     }
